47b10bd9d159954b5f15f7e8a198adbfedc98345
shubhamprakashrai <100286749+shubhamprakashrai@users.noreply.github.com>
2023-02-22 15:12:31 +0530

    login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+    function login(Request $request)
+    {
+        $user= User::where(['email'=>$request->email])->first();
+        if( !$user || !Hash ::check($request->password,$user->password))
+        {
+            return"Username or password is not matched";
+        }
+        else {
+            $request->session()->put('user',$user);
+            return redirect('/');
+        }
+    }
+}
---
+++ database/seeders/User_Seeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class User_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('users')->insert(
+            [
+                'name'=>'shubham',
+                'email'=>"shubham@gmail.com",
+                 'password'=>Hash::make('1234')
+            ]
+            );
+    }
+}
